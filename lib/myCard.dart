7c164ebbb576da6a2da42e23b52536b0ae4cbef9
hyunkim <han.kim@kakao.com>
2022-03-25 13:39:49 +0900

    Padding()

---
+++ lib/myCard.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Character card", // 앱을 총칭하는 이름
+      home: MyCard(),
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BBANTO"),
+        centerTitle: true, // title 정렬 옵션
+        backgroundColor: Colors.redAccent,
+        elevation: 0, // 떠 있는 듯한 음영효과 위한 높이 설정
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 0, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, //앱 스크린 내 위젯을 상하단중간에 위치시킬 때
+          children: [
+            Text("hello"),
+            Text("hello"),
+            Text("hello")
+          ],
+        ),
+      )
+    );// Scaffold : 앱 화면에 다양한 요소를 배치하고 그릴 수 있도록 도와주는 빈 도화지 같은 역할
+  }
+}
